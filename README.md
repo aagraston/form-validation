cc70ea836bdd4aa40f10f69afe203548c68633f0
Aaron graston <91895961+aagraston@users.noreply.github.com>
2022-02-28 09:18:34 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# boiler-plate-2.0
+A simple boiler plate for practice with Webdev setup as well as integration of ESlint and Prettier
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devtool: 'eval',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Your Title Here',
+      template: './src/template.html',
+      filename: './index.html',
+      inject: true,
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss/i,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+}
