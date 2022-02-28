a63156483e1575471fbf508c6cceabf6f84ff97b
aagraston <aagraston@gmail.com>
2022-02-28 12:44:23 -0800

    html validation added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './scss/app.scss'
+import Countries from './countries'
+
+// select input elements
+const emailInput = document.querySelector('#input-email')
+const countryInput = document.querySelector('#input-country')
+const postalInput = document.querySelector('#input-postal-code')
+const passwordInput = document.querySelector('#input-password')
+const confirmPasswordInput = document.querySelector('#input-confirm-password')
